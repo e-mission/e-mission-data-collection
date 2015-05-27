6a469a1df36e18727eef0aa1556a59ff9c340890
K. Shankari <shankari@eecs.berkeley.edu>
2015-05-26 23:57:04 -0700

    Simplify end to end development by skipping authorization
    When we are connected to a development server, skip the creation of the JWT and
    just send the unencrypted email address instead. This makes it much easier to
    start end to end development. Unfortunately, for iOS data collection, we still
    need keys from parse, but that should not block most development.

---
+++ iOS/CFC_Tracker/CFC_Tracker/SkipAuthEmailViewController.h
@@ -0,0 +1,15 @@
+//
+//  SkipAuthEmailViewController.h
+//  CFC_Tracker
+//
+//  Created by Kalyanaraman Shankari on 5/26/15.
+//  Copyright (c) 2015 Kalyanaraman Shankari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SkipAuthEmailViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *userEmailField;
+
+@end
