35569690a78d54d20723cd5a04084bcfd668265a
K. Shankari <shankari@eecs.berkeley.edu>
2015-06-20 16:41:35 -0700

    Fix the long-term logging
    We had long-term logging turned on for a while, trying to log to a file.
    However, this didn't actually work, because the logger was reinitialized every
    time the cfc_tracker process as initialized, but the handler was not
    initialized because the preference was set.
    
    However, even after removing the shared preferences check, this didn't work
    because we would need to init the handler every time the logger was
    initialized, which means every time the process was launched.
    
    In order to avoid identifying all the potential entry points into the app, we
    simply initialize the logger lazily. In order to do this along with the
    FileHandler set, we need to change the log() method signatures to take a context.
    
    This commit makes that change, and also provides convenience functions to use
    the logger and to return log statements for display in the app.

--- android/app/src/main/java/edu/berkeley/eecs/cfc_tracker/Log.java
+++ android/app/src/main/java/edu/berkeley/eecs/cfc_tracker/Log.java
@@ -2,11 +2,19 @@ package edu.berkeley.eecs.cfc_tracker;
 
 import android.content.Context;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Formatter;
+import java.util.Iterator;
+import java.util.PriorityQueue;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 /**
@@ -16,40 +24,143 @@ import java.util.logging.Logger;
  */
 
 public class Log {
-    private static final Logger logger =
-            Logger.getLogger("edu.berkeley.eecs.cfc_tracker");
+    public static String logFilePrefix = "long-term";
+    private static Logger logger;
+    private static final int MB = 1024 * 1024;
 
-    public static void initWithFile(Context ctxt) throws IOException {
-        FileHandler fh = new FileHandler(ctxt.getFilesDir()+"long-term.log");
-        logger.addHandler(fh);
+    public static java.util.logging.Formatter simpleFormatter = new java.util.logging.Formatter() {
+        @Override
+        public String format(LogRecord logRecord) {
+            return "[" + logRecord.getSequenceNumber() + "|" + logRecord.getMillis() + "|" + logRecord.getLevel() +"]" +
+                    logRecord.getMessage() + "\n";
+        }
+    };
+
+    public static FilenameFilter simpleFilter = new java.io.FilenameFilter() {
+        @Override
+        public boolean accept(File file, String s) {
+            if (s.startsWith(logFilePrefix) && s.endsWith(".log")) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+
+
+
+    class LogLinesIterator implements Iterator<String> {
+        private File[] sortedList;
+        private int currIndex = 0;
+        private BufferedReader currReader = null;
+        private String nextLine = null;
+
+        protected LogLinesIterator(Context ctxt) throws IOException {
+            File[] selectedFiles = ctxt.getFilesDir().listFiles(simpleFilter);
+            System.out.println("selectedFiles = "+java.util.Arrays.toString(selectedFiles));
+            sortedList = new File[selectedFiles.length];
+            for (int i = 0; i < selectedFiles.length - 1; i++) {
+                sortedList[i] = selectedFiles[i+1];
+            }
+            sortedList[selectedFiles.length] = selectedFiles[0];
+            if (sortedList.length > 0) {
+                currReader = new BufferedReader(new FileReader(sortedList[currIndex]));
+                nextLine = getNextLine();
+            }
+        }
+
+        private String getNextLine() throws IOException {
+            String nextLineInFile = currReader.readLine();
+            if (nextLineInFile != null) {
+                // There are more lines in this file, we are can just return them
+                return nextLineInFile;
+            } else {
+                // This could either be the end of this file, or the end of all files.
+                // Let's try to figure out which
+                currIndex = currIndex + 1;
+                if (currIndex < sortedList.length) {
+                    // There are more files, so we can start reading from the next one
+                    currReader = new BufferedReader(new FileReader(sortedList[currIndex]));
+                    return currReader.readLine();
+                } else {
+                    // This was the last file, and we got to the end of it, so we are done
+                    return null;
+                }
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nextLine != null;
+        }
+
+        @Override
+        public String next() {
+            String toReturn = nextLine;
+            try {
+                nextLine = getNextLine();
+            } catch (IOException e) {
+                nextLine = null;
+            }
+            return toReturn;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("LogLinesIterator does not support removal");
+        }
     }
 
-    public static Logger getLogger() {
+    public static String extractMessage(String logLine) {
+        return logLine.split("]")[1];
+    }
+
+    public static String getPattern(Context ctxt, String logFilePrefix) {
+        String pattern = ctxt.getFilesDir()+"/"+logFilePrefix+"-%g.log";
+        System.out.println("Returning pattern "+pattern);
+        return pattern;
+    }
+
+    public static Logger getLogger(Context ctxt) {
+        if (logger == null) {
+            System.out.println("logger == null, lazily creating new logger");
+            logger = Logger.getLogger("edu.berkeley.eecs.cfc_tracker");
+            String pattern = getPattern(ctxt, logFilePrefix);
+            System.out.println("Initializing file handler with pattern " + pattern);
+            try {
+                FileHandler fh = new FileHandler(pattern, 10 * MB, 10); // 10 files of 10 MB each
+                fh.setFormatter(simpleFormatter);
+                logger.addHandler(fh);
+            } catch (IOException e) {
+                // TODO: generate notification here instead
+                System.out.println("Error "+e+" while creating file handler, logging is only to the short-term adb logcat");
+            }
+        }
+        System.out.println("Returning logger with "+logger.getHandlers().length+" handlers "+logger.getHandlers());
         return logger;
     }
 
-    public static void d(String TAG, String message) {
-        getLogger().log(Level.FINE,
+    public static void d(Context ctxt, String TAG, String message) {
+        getLogger(ctxt).log(Level.FINE,
                 String.format("%s : %s", TAG, message));
         android.util.Log.d(TAG, message);
     }
 
-    public static void i(String TAG, String message) {
-        getLogger().log(Level.INFO,
+    public static void i(Context ctxt, String TAG, String message) {
+        getLogger(ctxt).log(Level.INFO,
                 String.format("%s : %s", TAG, message));
         android.util.Log.i(TAG, message);
     }
 
-    public static void w(String TAG, String message) {
-        getLogger().log(Level.WARNING,
+    public static void w(Context ctxt, String TAG, String message) {
+        getLogger(ctxt).log(Level.WARNING,
                 String.format("%s : %s", TAG, message));
         android.util.Log.w(TAG, message);
     }
 
-    public static void e(String TAG, String message) {
-        getLogger().log(Level.SEVERE,
+    public static void e(Context ctxt, String TAG, String message) {
+        getLogger(ctxt).log(Level.SEVERE,
                 String.format("%s : %s", TAG, message));
         android.util.Log.e(TAG, message);
     }
-
 }
