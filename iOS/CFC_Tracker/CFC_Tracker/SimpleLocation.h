7a2024872a0d8c191e184aa8aef9ff3086663048
K. Shankari <shankari@eecs.berkeley.edu>
2015-11-01 07:30:06 -0800

    Wrapper classes for the data stored in the user cache
    These are all simple classes that are basically just a list of properties.
    They are intentionally similar to the corresponding wrapper classes on android.

---
+++ iOS/CFC_Tracker/CFC_Tracker/SimpleLocation.h
@@ -0,0 +1,30 @@
+//
+//  SimpleLocation.h
+//  CFC_Tracker
+//
+//  Created by Kalyanaraman Shankari on 10/27/15.
+//  Copyright © 2015 Kalyanaraman Shankari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CLLocation.h>
+
+@interface SimpleLocation : NSObject
+
+- (instancetype)initWithCLLocation:(CLLocation *)loc;
+
+@property double latitude;
+@property double longitude;
+@property double altitude;
+
+@property double ts;
+@property NSString* fmt_time;
+
+@property double sensed_speed;
+@property double accuracy;
+@property double vaccuracy;
+@property double bearing;
+
+@property NSInteger floor;
+
+@end
