16187bf813c5bdc137a4de02ab6798614b6e6fae
K. Shankari <shankari@eecs.berkeley.edu>
2016-01-02 23:32:16 -0800

    Checking the geofence actions back in
    So that I can merge properly from the upstream

---
+++ iOS/CFC_Tracker/CFC_Tracker/GeofenceActions.h
@@ -0,0 +1,13 @@
+//
+//  GeofenceActions.h
+//  CFC_Tracker
+//
+//  Created by Kalyanaraman Shankari on 11/27/15.
+//  Copyright © 2015 Kalyanaraman Shankari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GeofenceActions : NSObject
+
+@end
---
+++ iOS/CFC_Tracker/CFC_Tracker/GeofenceActions.m
@@ -0,0 +1,13 @@
+//
+//  GeofenceActions.m
+//  CFC_Tracker
+//
+//  Created by Kalyanaraman Shankari on 11/27/15.
+//  Copyright © 2015 Kalyanaraman Shankari. All rights reserved.
+//
+
+#import "GeofenceActions.h"
+
+@implementation GeofenceActions
+
+@end
