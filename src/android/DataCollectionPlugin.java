ec4cc32830d87266cccc7e03f0ca3e6c4b7c83dc
K. Shankari <shankari@eecs.berkeley.edu>
2016-02-03 22:52:57 -0800

    Minor formatting fixes to the plugin interface
    I also radically simplified the google play services check since it looks like
    the android sample code does not do the more complex check any more anyway, and
    fixed the async invocation.

--- src/android/DataCollectionPlugin.java
+++ src/android/DataCollectionPlugin.java
@@ -5,17 +5,33 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationRequest;
+
+import edu.berkeley.eecs.emission.R;
+import edu.berkeley.eecs.emission.cordova.tracker.location.LocationTrackingConfig;
+import edu.berkeley.eecs.emission.cordova.unifiedlogger.Log;
+
 public class DataCollectionPlugin extends CordovaPlugin {
+    public static String TAG = "DataCollectionPlugin";
+
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("startupInit")) {
-            if (GooglePlayChecker.servicesConnected(cordova.getActivity()) {
-                sendBroadcast(new Intent(getString(R.string.transition_initialize)));
+            Activity myActivity = cordova.getActivity();
+            int connectionResult = GooglePlayServicesUtil.isGooglePlayServicesAvailable(myActivity);
+            if (connectionResult == ConnectionResult.SUCCESS) {
+                Log.d(myActivity, TAG, "google play services available, initializing state machine");
+                myActivity.sendBroadcast(new Intent(myActivity.getString(R.string.transition_initialize)));
                 callbackContext.success();
             } else {
-                Log.e(cordova.getActivity(), TAG, "unable to connect to google play services");
+                Log.e(myActivity, TAG, "unable to connect to google play services");
+                callbackContext.error(connectionResult);
             }
             return true;
         } else if (action.equals("launchInit")) {
@@ -26,8 +42,8 @@ public class DataCollectionPlugin extends CordovaPlugin {
             Context ctxt = cordova.getActivity();
             LocationTrackingConfig cfg = LocationTrackingConfig.getConfig(ctxt);
 
-            JSONObject retObject = JSONObject();
-            retObject.put("isDutyCycling", LocationTrackingConfig.getConfig(ctxt).isDutyCycling();
+            JSONObject retObject = new JSONObject();
+            retObject.put("isDutyCycling", LocationTrackingConfig.getConfig(ctxt).isDutyCycling());
             retObject.put("accuracy", getAccuracyAsString(cfg.getAccuracy()));
             retObject.put("geofenceRadius", cfg.getRadius());
             retObject.put("accuracyThreshold", cfg.getAccuracyThreshold());
@@ -49,11 +65,5 @@ public class DataCollectionPlugin extends CordovaPlugin {
         } else {
             return "UNKNOWN";
         }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(cordova.getActivity(), "TAG, requestCode = "+requestCode+" resultCode = "+resultCode);
-        GooglePlayChecker.onActivityResult(requestCode, resultCode, data);
     }
 }
