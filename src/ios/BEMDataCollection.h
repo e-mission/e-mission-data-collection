24cb40018f071a48e0a662e9538c56fed63759bb
K. Shankari <shankari@eecs.berkeley.edu>
2016-02-26 16:23:26 -0800

    Add a new plugin initialize method
    The new method does the following:
    - Create a TravelDiaryStateMachine instance if needed and store a strong
      reference to it so that it is not garbage collected.
    - Call the existing didFinishLaunchingWithOptions method in the AppDelegate
      with empty options since we don't actually do anything different based on
      them.

--- src/ios/BEMDataCollection.h
+++ src/ios/BEMDataCollection.h
@@ -1,12 +1,16 @@
 #import <Cordova/CDV.h>
+#import "TripDiaryStateMachine.h"
 
 @interface BEMDataCollection: CDVPlugin
 
+- (void) pluginInitialize;
 - (void) launchInit:(CDVInvokedUrlCommand*)command;
 - (void) getConfig:(CDVInvokedUrlCommand*)command;
 - (void) getState:(CDVInvokedUrlCommand*)command;
 - (void) forceTripStart:(CDVInvokedUrlCommand *)command;
 - (void) forceTripEnd:(CDVInvokedUrlCommand *)command;
 - (void) forceRemotePush:(CDVInvokedUrlCommand *)command;
+
+@property (strong) TripDiaryStateMachine* tripDiaryStateMachine;
 
 @end
