8183867aa791b198692eacd390c6b59240268a6e
K. Shankari <shankari@eecs.berkeley.edu>
2016-03-27 17:32:54 -0700

    Log battery level on each sync + notify each time
    This time, we have vibration turned on, so that the screen comes on, and the
    phone vibrates, thus hopefully simulating user interaction.

--- src/android/sensors/BatteryPollSensor.java
+++ src/android/sensors/BatteryPollSensor.java
@@ -3,6 +3,7 @@ package edu.berkeley.eecs.emission.cordova.tracker.sensors;
 import android.content.Context;
 
 import edu.berkeley.eecs.emission.R;
+import edu.berkeley.eecs.emission.cordova.tracker.wrapper.Battery;
 import edu.berkeley.eecs.emission.cordova.usercache.UserCacheFactory;
 
 /**
@@ -10,7 +11,7 @@ import edu.berkeley.eecs.emission.cordova.usercache.UserCacheFactory;
  */
 public class BatteryPollSensor implements PollSensor {
     public void getAndSaveValue(Context ctxt) {
-        float currLevel = BatteryUtils.getBatteryLevel(ctxt);
-        UserCacheFactory.getUserCache(ctxt).putSensorData(R.string.key_usercache_battery, currLevel);
+        Battery currBattery = BatteryUtils.getBatteryInfo(ctxt);
+        UserCacheFactory.getUserCache(ctxt).putSensorData(R.string.key_usercache_battery, currBattery);
     }
 }
--- src/android/sensors/BatteryUtils.java
+++ src/android/sensors/BatteryUtils.java
@@ -3,18 +3,13 @@ package edu.berkeley.eecs.emission.cordova.tracker.sensors;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.BatteryManager;
+
+import edu.berkeley.eecs.emission.cordova.tracker.wrapper.Battery;
 
 public class BatteryUtils {
-	public static float getBatteryLevel(Context applicationContext) {
-	    Intent batteryIntent = applicationContext.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-	    int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-	    int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-
-	    // Error checking that probably isn't needed but I added just in case.
-	    if(level == -1 || scale == -1) {
-	        return 50.0f;
-	    }
-	    return ((float)level / (float)scale) * 100.0f; 
+	public static Battery getBatteryInfo(Context applicationContext) {
+	    Intent batteryIntent = applicationContext.registerReceiver(null,
+				new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+		return new Battery(batteryIntent);
 	}
 }
