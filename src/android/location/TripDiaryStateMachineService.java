454f00982c24cf9f4113452dbb5a8ede15de7ec0
Shankari <shankari@eecs.berkeley.edu>
2020-08-06 14:32:32 -0700

    Fix import

--- src/android/location/TripDiaryStateMachineService.java
+++ src/android/location/TripDiaryStateMachineService.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 
