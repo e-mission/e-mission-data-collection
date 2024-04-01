9fb3935aae6c40390b3fb921f7d0d3a5cb0d2b48
louisg1337 <louisgrassi1337@gmail.com>
2024-03-31 22:14:43 -0400

    Fixed BluetoothServie so that it executes a few ranges before returning if it has detected a beacon in the region. Also started setting up adding a new state in the FSM.

--- src/android/bluetooth/BluetoothService.java
+++ src/android/bluetooth/BluetoothService.java
@@ -4,11 +4,17 @@ package edu.berkeley.eecs.emission.cordova.tracker.bluetooth;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import java.util.Collection;
+import java.util.concurrent.CountDownLatch;
+import java.util.Set;
+import java.util.HashSet;
 
 // Altbeacon imports
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Beacon;
 
 // Other plugin imports
 import edu.berkeley.eecs.emission.cordova.unifiedlogger.Log;
@@ -17,6 +23,7 @@ import edu.berkeley.eecs.emission.cordova.unifiedlogger.Log;
 public class BluetoothService extends Service {
     private static String TAG = "BluetoothService";
     private BeaconManager beaconManager;
+    private Set<Beacon> scanned = new HashSet<>();
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -38,44 +45,62 @@ public class BluetoothService extends Service {
         return null;
     }
 
+    private void isInRange() {
+        Log.d(this, TAG, "Is in range has been called!");
+
+        stopBeaconScan();
+
+        Log.d(this, TAG, "Done waiting, results are... " + scanned.size());
+
+        if (scanned.size() > 0) {
+            Log.d(this, TAG, "Found something!");
+            Log.d(this, TAG, scanned.toString());
+        } else {
+            Log.d(this, TAG, "Did not find anything!");
+        }
+    }
+
     private void startBeaconScan() {
         // Code to start scanning for BLE beacons using AltBeacon library
         Log.d(this, TAG, "startBeaconScan called!!!!");
 
-        beaconManager.addMonitorNotifier(new MonitorNotifier() {
+        // Keep track of how many times we have scanned
+
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            int numScans = 0;
+
             @Override
-            public void didEnterRegion(Region region) {
-                Log.d(BluetoothService.this, TAG, "Beacon entered the region. Beacon UID: " + region.getUniqueId());
-            }
-    
-            @Override
-            public void didExitRegion(Region region) {
-                Log.d(BluetoothService.this, TAG, "Beacon has exited the region. Beacon UID: " + region.getUniqueId());
-            }
-    
-            @Override
-            public void didDetermineStateForRegion(int state, Region region) {
-                // state = 0, OUTSIDE
-                // state = 1, INSIDE
-                if (state == 0) {
-                    Log.d(BluetoothService.this, TAG, "Beacon is not inside the region! Beacon UID: " + region.getUniqueId());
-                } else {
-                    Log.d(BluetoothService.this, TAG, "Beacon is inside the region! Beacon UID: " + region.getUniqueId());
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.d(BluetoothService.this, TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+
+                    for (Beacon beacon : beacons) {
+                        Log.d(BluetoothService.this, TAG, beacon.toString());
+                        scanned.add(beacon);
+                    }
+                }
+
+                numScans++;
+
+                if (numScans >= 4) {
+                    // Once we have hit certain number of scans, stop and determine if any beacons are in range
+                    isInRange();
                 }
             }
         });
     
-        beaconManager.startMonitoring(new Region("426C7565-4368-6172-6D42-6561636F6E73", null, null, null));
+        beaconManager.startRangingBeacons(new Region("426C7565-4368-6172-6D42-6561636F6E73", null, null, null));
     }
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
         stopBeaconScan();
+        super.onDestroy();
     }
 
     private void stopBeaconScan() {
         Log.d(this, TAG, "Stopping monitoring for the beacon.");
-        beaconManager.stopMonitoring(new Region("426C7565-4368-6172-6D42-6561636F6E73", null, null, null));
+        beaconManager.stopRangingBeacons(new Region("426C7565-4368-6172-6D42-6561636F6E73", null, null, null));
+        beaconManager.removeAllRangeNotifiers();
     }
 }
