01c746e35284de284fe3a5bcade47c41f43d9948
Shankari <shankari@eecs.berkeley.edu>
2024-08-28 13:56:08 -0700

    🔒️ Restrict the geofence location event to the current package
    This is one of the security recommendations
    https://developer.android.com/develop/background-work/background-tasks/broadcasts#java
    
    I also tried to change the receiver to be NOT_EXPORTED, but could not get that
    to work in the limited time available.
    
    I also tried to send the message via an Intent that specified the context and
    the class, but that does not work with broadcast intents
    https://github.com/e-mission/e-mission-docs/issues/1079#issuecomment-2314436185
    
    Testing done:
    - Before the change
    
    ```
    08-27 22:36:18.466  4901  5581 I GeofenceLocationIntentService: broadcasting intent Intent { act=GEOFENCE_LOCATION_RESULT (has extras) }
    ```
    
    - After the change
    
    ```
    08-27 22:42:29.312  6785  7442 I GeofenceLocationIntentService: broadcasting intent Intent { act=GEOFENCE_LOCATION_RESULT pkg=edu.berkeley.eecs.emission (has extras) }
    ```

--- src/android/location/actions/GeofenceLocationIntentService.java
+++ src/android/location/actions/GeofenceLocationIntentService.java
@@ -112,6 +112,7 @@ public class GeofenceLocationIntentService extends IntentService {
 
     private void broadcastLoc(Location loc) {
         Intent answerIntent = new Intent(INTENT_NAME);
+        answerIntent.setPackage(this.getPackageName());
         answerIntent.putExtra(INTENT_RESULT_KEY, loc);
         Log.i(this, TAG, "broadcasting intent "+answerIntent);
         LocalBroadcastManager.getInstance(this).sendBroadcast(answerIntent);
